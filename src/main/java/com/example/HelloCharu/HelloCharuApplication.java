932acbbe9299a23108d9b19e41131b172a5184bd
Charu Sharma <CSharma6@slb.com>
2021-07-16 16:00:34 +0530

    shdu

---
+++ src/main/java/com/example/HelloCharu/HelloCharuApplication.java
@@ -0,0 +1,15 @@
+package com.example.HelloCharu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCharuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCharuApplication.class, args);
+	
+
+	}
+
+}
---
+++ src/test/java/com/example/HelloCharu/HelloCharuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloCharu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCharuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
